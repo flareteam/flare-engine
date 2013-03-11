1b5bfb1be81e5a32673c3430d1fc93d477b692bc
Joseph Bleau <jbleau@gmail.com>
2013-03-11 03:55:20 -0400

    Added TabList class which can be utilized anywhere to maintain appropriate keyboard control over widgets.

--- src/Widget.cpp
+++ src/Widget.cpp
@@ -1,5 +1,6 @@
 /*
 Copyright © 2012 Stefan Beller
+Copyright © 2013 Joseph Bleau
 
 This file is part of FLARE.
 
@@ -19,6 +20,8 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 
 Widget::Widget()
 	: render_to_alpha(false)
+	, in_focus(false)
+	, focusable(false)
 	, pos(SDL_Rect())
 {
 	pos.x = pos.y = pos.w = pos.h = 0;
@@ -27,3 +30,76 @@ Widget::Widget()
 Widget::~Widget()
 {}
 
+void Widget::activate()
+{}
+
+TabList::TabList()
+	: widgets()
+	, current(-1)
+{}
+
+TabList::~TabList()
+{}
+
+void TabList::add(Widget* widget) {
+	if( widget == NULL )
+		return;
+
+	widgets.push_back(widget);
+}
+
+void TabList::remove(Widget* widget) {
+	std::vector<Widget*>::iterator find = std::find(
+		widgets.begin(),
+		widgets.end(),
+		widget
+	);
+
+	if(find != widgets.end())
+		widgets.erase(find);
+}
+
+Widget* TabList::getNext() {
+	if(widgets.size() == 0)
+		return NULL;
+
+	if(current >= 0 && current < widgets.size())
+		widgets.at(current)->in_focus = false;
+
+	++current;
+
+	if(current >= widgets.size())
+		current = 0;
+
+	widgets.at(current)->in_focus = true;
+	return widgets.at(current);
+}
+
+Widget* TabList::getPrev() {
+	if(widgets.size() == 0)
+		return NULL;
+
+	if(current >= 0 && current < widgets.size())
+		widgets.at(current)->in_focus = false;
+
+	--current;
+
+	if(current <= -1)
+		current = widgets.size()-1;
+
+	widgets.at(current)->in_focus = true;
+	return widgets.at(current);
+}
+
+void TabList::activate() {
+	if(current >= 0 && current < widgets.size()) {
+		widgets.at(current)->activate();
+	}
+}
+
+void TabList::defocus() {
+	if(current >= 0 && current < widgets.size())
+		widgets.at(current)->in_focus = false;
+
+	current = -1;
+}
