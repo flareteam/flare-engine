967479f428fa3d32dc069d22ce9363380bb30e0d
Stefan Beller <stefanbeller@googlemail.com>
2013-04-27 18:07:48 +0200

    Implement new FileParser to work according to specified documentation
    Signed-off-by: Stefan Beller <stefanbeller@googlemail.com>

--- src/FileParser.h
+++ src/FileParser.h
@@ -28,20 +28,20 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 
 #include <fstream>
 #include <string>
+#include <vector>
 
 class FileParser {
 private:
-	std::string filename; // generic filename
-	std::string current_filename; // localized by mod manager
+	std::vector<std::string> filenames;
+	unsigned current_index;
+	std::string errormessage;
+
 	std::ifstream infile;
 	std::string line;
 
 public:
 	FileParser();
 	~FileParser();
-
-
-	bool openDirect(const std::string& filename, const std::string &errormessage = "Could not open text file");
 
 	/**
 	 * @brief open
@@ -61,11 +61,15 @@ public:
 	 * actual filename before opening the file. It is true by default.
 	 * If this is set to false, then the filename is interpreted as is.
 	 *
+	 * @param stopAfterFirstFile
+	 * If this is set, the newest file is being read. This is the default.
+	 * If it is set to false, all possible files are parsed and delivered as
+	 * one big stream. Also if using direct directory pathes, these will be
+	 * expanded and parsed.
+	 *
 	 * @return true if file could be opened successfully for reading.
 	 */
-	bool openLocated(const std::string& filename, const std::string &errormessage = "Could not open text file", locateFileName = true);
-
-	bool openAllModsSerialized(const std::string& filename, const std::string &errormessage = "Could not open text file");
+	bool openLocated(const std::string& filename, bool locateFileName = true, bool stopAfterFirstFile = true, const std::string &errormessage = "Could not open text file");
 
 	void close();
 	bool next();
