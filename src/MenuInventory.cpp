654505a861205a45dfc63f9add55023fa5a5e294
Justin Jacobs <jajdorkster@gmail.com>
2013-05-03 14:44:42 -0400

    Fix off-by-one error

--- src/MenuInventory.cpp
+++ src/MenuInventory.cpp
@@ -180,7 +180,7 @@ void MenuInventory::logic() {
 				}
 			}
 			if (!removable_items.empty()) {
-				int random_item = (rand() % removable_items.size()) - 1;
+				int random_item = rand() % removable_items.size();
 				remove(removable_items[random_item]);
 				death_message += msg->get("Lost %s.",items->items[removable_items[random_item]].name);
 			}
