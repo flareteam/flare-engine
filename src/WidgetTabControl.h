6a5490bab8b362a6abc765ee7527dd466bae6b22
Stefan Beller <stefanbeller@googlemail.com>
2013-05-06 18:34:13 +0200

    WidgetTabControl: Remove unused function getTabsAmount

--- src/WidgetTabControl.h
+++ src/WidgetTabControl.h
@@ -60,7 +60,6 @@ public:
 
 	int getActiveTab();
 	void setActiveTab(int tab) { activeTab = tab; }
-	int getTabsAmount();
 	SDL_Rect getContentArea();
 	int getTabHeight() { return activeTabSurface->h; }
 
