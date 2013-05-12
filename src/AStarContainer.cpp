45deade95e61bbde38b8aa64b43e7815e1d299b8
Ryan Dansie <ryan_dansie@yahoo.co.uk>
2013-05-12 21:53:37 +0100

    First version of container for open nodes

---
+++ src/AStarContainer.cpp
@@ -0,0 +1,121 @@
+#include "AStarContainer.h"
+#include <cstring>
+
+AStarContainer::AStarContainer(unsigned int map_width, unsigned int map_height, unsigned int node_limit)
+    : size(0)
+    , map_width(0)
+{
+    this->map_width = map_width;
+
+    nodes = new AStarNode*[node_limit];
+    map_pos = new int[map_width * map_height];
+
+    //initialise the map array. A -1 value will mean there is no node at that position
+    std::memset(map_pos, -1, sizeof(int) * (map_width + map_height));
+}
+
+AStarContainer::~AStarContainer()
+{
+    for(int i=0; i<size; i++)
+        delete nodes[i];
+    delete [] nodes;
+    delete [] map_pos;
+}
+
+void AStarContainer::add(AStarNode* node){
+    nodes[size] = node;
+
+    //reorder the heap
+    int m = size;
+    AStarNode* temp = NULL;
+    while(m != 0){
+        if(nodes[m]->getFinalCost() <= nodes[m/2]->getFinalCost()){
+            temp = nodes[m/2];
+            nodes[m/2] = nodes[m];
+            map_pos[nodes[m/2]->getX() + nodes[m/2]->getY() * map_width] = m/2;
+            nodes[m] = temp;
+            map_pos[nodes[m]->getX() + nodes[m]->getY() * map_width] = m;
+            m=m/2;
+        }
+        else
+            break;
+    }
+    size++;
+}
+
+AStarNode* AStarContainer::get_shortest_f(){
+    return nodes[0];
+}
+
+void AStarContainer::remove(AStarNode* node){
+
+    int heap_indexv = map_pos[node->getX() + node->getY() * map_width];
+
+    //swap the last node in the list with the node being deleted
+    nodes[heap_indexv] = nodes[size];
+    size--;
+
+    //reorder the heap
+    int heap_indexu = heap_indexv;
+
+    while(true){
+        heap_indexu = heap_indexv;
+        if(2*heap_indexu+1 <= size){//if both children exist
+            //Select the lowest of the two children.
+            if(nodes[heap_indexu]->getFinalCost() >= nodes[2*heap_indexu]->getFinalCost()) heap_indexv = 2*heap_indexu;
+            if(nodes[heap_indexv]->getFinalCost() >= nodes[2*heap_indexu+1]->getFinalCost()) heap_indexv = 2*heap_indexu+1;
+        }
+        else if (2*heap_indexu <= size){//if only child #1 exists
+            //Check if the F cost is greater than the child
+            if(nodes[heap_indexu]->getFinalCost() >= nodes[2*heap_indexu]->getFinalCost()) heap_indexv = 2*heap_indexu;
+        }
+
+        if(heap_indexu != heap_indexv){//If parent's F > one or both of its children, swap them
+            AStarNode* temp = nodes[heap_indexu];
+            nodes[heap_indexu] = nodes[heap_indexv];
+            nodes[heap_indexv] = temp;
+        }
+        else{
+            break;//if item <= both children, exit loop
+        }
+    }//Repeat forever
+
+    //remove the node from the map array
+    map_pos[node->getX() + node->getY() * map_width] = -1;
+}
+
+bool AStarContainer::exists(Point pos){
+    return map_pos[pos.x + pos.y * map_width] != -1;
+}
+
+AStarNode* AStarContainer::get(int x, int y){
+    return nodes[map_pos[x + y * map_width]];
+}
+
+bool AStarContainer::isEmpty(){
+    return size == 0;
+}
+
+void AStarContainer::updateParent(Point pos, Point parent_pos, float score){
+    get(pos.x, pos.y)->setParent(parent_pos);
+    get(pos.x, pos.y)->setActualCost(score);
+
+    //reorder the heap
+    int m = map_pos[pos.x + pos.y * map_width];
+    AStarNode* temp = NULL;
+    while(m != 0){
+        if(nodes[m]->getFinalCost() <= nodes[m/2]->getFinalCost()){
+            temp = nodes[m/2];
+            nodes[m/2] = nodes[m];
+            map_pos[nodes[m/2]->getX() + nodes[m/2]->getY() * map_width] = m/2;
+            nodes[m] = temp;
+            map_pos[nodes[m]->getX() + nodes[m]->getY() * map_width] = m;
+            m=m/2;
+        }
+        else
+            break;
+    }
+
+    map_pos[pos.x + pos.y * map_width] = m;
+}
+
---
+++ src/AStarContainer.h
@@ -0,0 +1,34 @@
+#ifndef ASTARCONTAINER_H
+#define ASTARCONTAINER_H
+
+#include "AStarNode.h"
+
+//designed to be used for the Open nodes. Unsuitable for Closed nodes
+class AStarContainer
+{
+    public:
+        AStarContainer(unsigned int map_width, unsigned int map_height, unsigned int node_limit);
+        ~AStarContainer();
+        //assumes that the node is not already in the collection
+        void add(AStarNode* node);
+        AStarNode* get_shortest_f();
+        //assumes that the node exists in the collection
+        void remove(AStarNode* node);
+        bool exists(Point pos);
+        //assumes that the node exists in the collection
+        AStarNode* get(int x, int y);
+        bool isEmpty();
+        //sorts the heap based on updated pos
+        void updateParent(Point pos, Point parent_pos, float score);
+
+    private:
+        unsigned int size;
+        unsigned int map_width;
+        //AStarNode array -- binary heap ordered
+        AStarNode** nodes;
+
+        //2d array linking map pos to the index needed for the nodes array
+        int* map_pos;
+};
+
+#endif // ASTARCONTAINER_H
