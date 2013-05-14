76864282863b93c03e852e19c0765726c4758fa1
Stefan Beller <stefanbeller@googlemail.com>
2013-05-14 10:59:41 +0200

    Add warning if not all enemies of an enemygroup could be spawned

--- src/MapRenderer.cpp
+++ src/MapRenderer.cpp
@@ -118,6 +118,9 @@ void MapRenderer::push_enemy_group(Map_Group g) {
 		else
 			allowed_misses--;
 	}
+	if (enemies_to_spawn)
+		fprintf(stderr, "Could not spawn all enemies in group at (x=%d,y=%d,w=%d,h=%d), %d missing\n",
+		g.pos.x, g.pos.y, g.area.x, g.area.y, enemies_to_spawn);
 }
 
 int MapRenderer::load(string filename) {
