201be6cc78df95b51b8cae2bc3f8cc9bec6baafc
Stefan Beller <stefanbeller@googlemail.com>
2013-05-22 08:36:14 +0200

    Render Maps correctly on disabled animated tiles as well

--- src/MapRenderer.cpp
+++ src/MapRenderer.cpp
@@ -435,7 +435,7 @@ void MapRenderer::renderIso(vector<Renderable> &r, vector<Renderable> &r_dead) {
 			SDL_FillRect(backgroundsurface, 0, 0);
 			Point off(VIEW_W_HALF, VIEW_H_HALF);
 			for (unsigned i = 0; i < index_objectlayer; ++i)
-					renderIsoLayer(screen, off, layers[i]);
+					renderIsoLayer(backgroundsurface, off, layers[i]);
 		}
 		Point p = map_to_screen(shakycam.x, shakycam.y , backgroundsurfaceoffset.x, backgroundsurfaceoffset.y);
 		SDL_Rect src;
