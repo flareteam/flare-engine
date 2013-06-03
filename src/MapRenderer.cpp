flareteam/flare-engine

commit 8c11ccf3a18031ebc8cf9b0dd1e0872e678ae8d0

Fix collision being off by half a tile
Note: This causes a bug with render order. My best guess is that there
is a round() that can be replaced with floor(), but I have no idea
where.

src/MapRenderer.cpp
@@ -588,8 +588,8 @@ void MapRenderer::executeOnMapExitEvents() {
 
 void MapRenderer::checkEvents(FPoint loc) {
 	Point maploc;
-	maploc.x = round(loc.x);
-	maploc.y = round(loc.y);
+	maploc.x = floor(loc.x);
+	maploc.y = floor(loc.y);
 	vector<Map_Event>::iterator it;
 
 	// loop in reverse because we may erase elements

src/MapCollision.cpp
@@ -51,6 +51,10 @@ void MapCollision::setmap(const unsigned short _colmap[][256], unsigned short w,
  * If we encounter an obstacle at 45 or 135 degrees, slide.
  */
 bool MapCollision::move(float &x, float &y, float _step_x, float _step_y, MOVEMENTTYPE movement_type, bool is_hero) {
+	// when trying to slide against a bottom or right wall, step_x or step_y can become 0
+	// this causes diag to become false, making this function return false
+	// we try to catch such a scenario and return true early
+	bool force_slide = (_step_x != 0 && _step_y > 0) || (_step_x > 0 && _step_y != 0);
 
 	while (_step_x != 0 || _step_y != 0) {
 
@@ -84,13 +88,16 @@ bool MapCollision::move(float &x, float &y, float _step_x, float _step_y, MOVEME
 			x += step_x;
 			y += step_y;
 		}
-		else if (diag && is_valid_position(x + step_x, y, movement_type, is_hero)) { // slide along wall
+		else if ((diag || force_slide) && is_valid_position(x + step_x, y, movement_type, is_hero)) { // slide along wall
 			x += step_x;
+			if (!diag && force_slide) return true;
 		}
-		else if (diag && is_valid_position(x, y + step_y, movement_type, is_hero)) { // slide along wall
+		else if ((diag || force_slide)  && is_valid_position(x, y + step_y, movement_type, is_hero)) { // slide along wall
 			y += step_y;
+			if (!diag && force_slide) return true;
 		}
-		else { // is there a singular obstacle or corner we can step around?
+		else {
+			// is there a singular obstacle or corner we can step around?
 			// only works if we are moving straight
 
 			if (diag) return false;
@@ -179,7 +186,7 @@ bool MapCollision::is_valid_tile(int tile_x, int tile_y, MOVEMENTTYPE movement_t
  * Is this a valid position for an entity with this movement type?
  */
 bool MapCollision::is_valid_position(float x, float y, MOVEMENTTYPE movement_type, bool is_hero) const {
-	return is_valid_tile(round(x), round(y), movement_type, is_hero);
+	return is_valid_tile(floor(x), floor(y), movement_type, is_hero);
 }
 
 bool inline MapCollision::is_sidestepable(int tile_x, int tile_y, int offx, int offy) {

commit 76e6229618b74692e1e7957847eec8b58557adfa

Use floor instead of render for Rendering.
This fixes problems as introduced by
previous commit 8c11ccf3a18031ebc8cf9b0dd1e0872e678ae8d0

src/MapRenderer.cpp
@@ -218,17 +218,17 @@ bool priocompare(const Renderable &r1, const Renderable &r2) {
  */
 void calculatePriosIso(vector<Renderable> &r) {
 	for (vector<Renderable>::iterator it = r.begin(); it != r.end(); ++it) {
-		const unsigned tilex = round(it->map_pos.x);
-		const unsigned tiley = round(it->map_pos.y);
-		const int commax = (float)(2<<16) * it->map_pos.x;
-		const int commay = (float)(2<<16) * it->map_pos.y;
-		it->prio += (((uint64_t)(tilex + tiley)) << 54) + (((uint64_t)tilex) << 45) + ((commax + commay) << 16);
+		const unsigned tilex = it->map_pos.x; // implicit floor, just taking the integer part of the float
+		const unsigned tiley = it->map_pos.y;
+		const int commax = (float)(it->map_pos.x - tilex) * (2<<16);
+		const int commay = (float)(it->map_pos.y - tiley) * (2<<16);
+		it->prio += (((uint64_t)(tilex + tiley)) << 54) + (((uint64_t)tilex) << 42) + ((commax + commay) << 16);
 	}
 }
 
 void calculatePriosOrtho(vector<Renderable> &r) {
 	for (vector<Renderable>::iterator it = r.begin(); it != r.end(); ++it) {
-		const unsigned tilex = round(it->map_pos.x);
+		const unsigned tilex = floor(it->map_pos.x);
 		const unsigned tiley = round(it->map_pos.y);
 		const int commay = 1024 * it->map_pos.y;
 		it->prio += (((uint64_t)tiley) << 48) + (((uint64_t)tilex) << 32) + (commay << 16);
@@ -363,7 +363,7 @@ void MapRenderer::renderIsoFrontObjects(vector<Renderable> &r) {
 	j = upperright.y - tset.max_size_y + tset.max_size_x;
 	i = upperright.x - tset.max_size_y - tset.max_size_x;
 
-	while (r_cursor != r_end && (round(r_cursor->map_pos.x) + round(r_cursor->map_pos.y) < i + j || round(r_cursor->map_pos.x) < i))
+	while (r_cursor != r_end && ((int)(r_cursor->map_pos.x) + (int)(r_cursor->map_pos.y) < i + j || (int)(r_cursor->map_pos.x) < i)) // implicit floor
 		++r_cursor;
 
 	maprow *objectlayer = layers[index_objectlayer];
@@ -400,7 +400,7 @@ void MapRenderer::renderIsoFrontObjects(vector<Renderable> &r) {
 			}
 
 			// some renderable entities go in this layer
-			while (r_cursor != r_end && (round(r_cursor->map_pos.x)) == i && (round(r_cursor->map_pos.y)) == j) {
+			while (r_cursor != r_end && ((int)r_cursor->map_pos.x == i && (int)r_cursor->map_pos.y == j)) { // implicit floor by int cast
 				drawRenderable(r_cursor);
 				++r_cursor;
 			}
@@ -412,7 +412,7 @@ void MapRenderer::renderIsoFrontObjects(vector<Renderable> &r) {
 		else
 			j++;
 
-		while (r_cursor != r_end && (round(r_cursor->map_pos.x) + round(r_cursor->map_pos.y) < i + j || round(r_cursor->map_pos.x) <= i))
+		while (r_cursor != r_end && ((int)r_cursor->map_pos.x + (int)r_cursor->map_pos.y < i + j || (int)r_cursor->map_pos.x <= i)) // implicit floor by int cast
 			++r_cursor;
 	}
 }