5b38ff391b4548cb8aa1f7a9670fbca3e33e099e
Henrik Andersson <hean01@users.sourceforge.net>
2013-06-07 23:39:17 +0200

    Added docs for combat text.

--- src/CombatText.cpp
+++ src/CombatText.cpp
@@ -44,12 +44,15 @@ CombatText::CombatText() {
 
 	// Load config settings
 	FileParser infile;
+	// @CLASS CombatText|Description of engine/combat_text.txt
 	if(infile.open("engine/combat_text.txt")) {
 		while(infile.next()) {
 			if(infile.key == "duration") {
+				// @ATTR duration|integer|Duration of the combat text.
 				duration = toInt(infile.val);
 			}
 			else if(infile.key == "speed") {
+				// @ATTR duration|integer|Motion speed of the combat text.
 				speed = toInt(infile.val);
 			}
 		}
