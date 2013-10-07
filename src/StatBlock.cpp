8f67e77b8e6c52e10b85c1d476649209ac6aa519
Ryan Dansie <ryan_dansie@yahoo.co.uk>
2013-10-07 22:57:04 +0100

    enemy convert_status

--- src/StatBlock.cpp
+++ src/StatBlock.cpp
@@ -272,6 +272,7 @@ void StatBlock::load(const string& filename) {
 			loot.push_back(el);
 		}
 		else if (infile.key == "defeat_status") defeat_status = infile.val;
+		else if (infile.key == "convert_status") convert_status = infile.val;
 		else if (infile.key == "first_defeat_loot") first_defeat_loot = num;
 		else if (infile.key == "quest_loot") {
 			quest_loot_requires = infile.nextValue();
