10687bfa4cc36b2afe643b388f32178746236bd8
Igor Paliychuk <mansonigor@gmail.com>
2014-08-12 15:52:13 +0300

    Add all required SDL libraries to build.

--- flare-android-project/src/org/libsdl/app/SDLActivity.java
+++ flare-android-project/src/org/libsdl/app/SDLActivity.java
@@ -49,10 +49,10 @@ public class SDLActivity extends Activity {
     // Load the .so
     static {
         System.loadLibrary("SDL2");
-        //System.loadLibrary("SDL2_image");
-        //System.loadLibrary("SDL2_mixer");
+        System.loadLibrary("SDL2_image");
+        System.loadLibrary("SDL2_mixer");
         //System.loadLibrary("SDL2_net");
-        //System.loadLibrary("SDL2_ttf");
+        System.loadLibrary("SDL2_ttf");
         System.loadLibrary("main");
     }
     
