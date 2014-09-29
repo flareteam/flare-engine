f427b001391a6de807be6f222bb4948e9c656fd0
Igor Paliychuk <mansonigor@gmail.com>
2014-09-29 19:10:24 +0300

    Android: update version. Add patch for resolution. update script

--- flare-android-project/server/insert_comment.sh
+++ flare-android-project/server/insert_comment.sh
@@ -3,5 +3,8 @@
 # run this from outside of mods folder
 
 for i in $( find  mods -type f -name '*.txt'); do
-	sed -i '1s/^/## android comment to workaround getline() issue\n/' $i
+	if ! grep -Fxq "## android comment to workaround getline() issue" $i
+	then
+		sed -i '1s/^/## android comment to workaround getline() issue\n/' $i
+	fi
 done
