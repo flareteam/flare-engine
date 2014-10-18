d0ecc7b0cd09853814a75cc26e5abe0d19589290
Stefan Beller <stefanbeller@gmail.com>
2014-10-17 19:36:33 -0700

    ItemManager: Add missing error(...) on parsing failure

--- src/ItemManager.cpp
+++ src/ItemManager.cpp
@@ -222,6 +222,8 @@ void ItemManager::loadItems() {
 				items[id].req_stat.push_back(REQUIRES_OFF);
 			else if (s == "defense")
 				items[id].req_stat.push_back(REQUIRES_DEF);
+			else
+				infile.error("%s unrecognized at; requires_stat must be one of [physical:mental:offense:defense]\n", s);
 			items[id].req_val.push_back(toInt(infile.nextValue()));
 		}
 		else if (infile.key == "bonus") {
