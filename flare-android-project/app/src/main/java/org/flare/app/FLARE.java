6b6a92bd21b0cd44048d1988ce8ae26f9ea677ea
Justin Jacobs <jajdorkster@gmail.com>
2015-08-11 06:13:37 -0400

    Re-add missing FLARE.java file to Android project
    This file not being added was caused by .gitignore not having leading
    slashes before many of the filenames.

---
+++ flare-android-project/app/src/main/java/org/flare/app/FLARE.java
@@ -0,0 +1,18 @@
+package org.flare.app; 
+
+import org.libsdl.app.SDLActivity; 
+import android.os.*; 
+
+/* 
+ * A sample wrapper class that just calls SDLActivity 
+ */ 
+
+public class FLARE extends SDLActivity { 
+    protected void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState);
+    } 
+    
+    protected void onDestroy() { 
+        super.onDestroy(); 
+    }
+}
