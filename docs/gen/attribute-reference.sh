ba4b7be908d08c3eaaa4064e128a2229d6b408ac
Justin Jacobs <jajdorkster@gmail.com>
2015-09-24 14:42:31 -0400

    Fix sed command in docs/gen/attribute-reference.sh
    This was causing improper output for strings that had the same character
    on either side of an underscore.
    
    Example of bad output:
    
    input = "combat_text"
    output = "combat_ext"

--- docs/gen/attribute-reference.sh
+++ docs/gen/attribute-reference.sh
@@ -6,7 +6,7 @@ PAGETITLE="Attribute Reference"
 sed -e "s/PAGETITLE/$PAGETITLE/g" header.txt > "$HTMLFILE"
 markdown "attribute-reference.md" >> "$HTMLFILE"
 cd ../../
-./extract_xml.sh | xsltproc wiki.xslt - | sed -e '1,3d' -e 's:\([a-z]*\)_\1:\1\\_:g' | markdown >> "docs/gen/$HTMLFILE"
+./extract_xml.sh | xsltproc wiki.xslt - | sed -e '1,3d' -e 's:\([a-z]*\)\1_:\1\\_:g' | markdown >> "docs/gen/$HTMLFILE"
 cd "docs/gen"
 cat footer.txt >> "$HTMLFILE"
 
