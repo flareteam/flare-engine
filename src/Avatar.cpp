bb24bea7626aba4aae3688d34fdaf991467707bb
Justin Jacobs <jajdorkster@gmail.com>
2016-01-03 10:42:37 -0500

    Fix bug where power wouldn't be marked as executed when it fails

--- src/Avatar.cpp
+++ src/Avatar.cpp
@@ -316,6 +316,9 @@ void Avatar::handlePower(std::vector<ActionData> &action_queue) {
 			// check requirements
 			if ((stats.cur_state == AVATAR_ATTACK || stats.cur_state == AVATAR_HIT) && !action.instant_item)
 				continue;
+
+			action.executed = true;
+
 			if (!stats.canUsePower(power, action.power))
 				continue;
 			if (power.requires_los && !mapr->collider.line_of_sight(stats.pos.x, stats.pos.y, target.x, target.y))
@@ -381,8 +384,6 @@ void Avatar::handlePower(std::vector<ActionData> &action_queue) {
 					}
 					break;
 			}
-
-			action.executed = true;
 		}
 	}
 
