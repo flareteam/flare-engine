e856bf00dfb749275b623b9ca61d612d7a81f73a
Justin Jacobs <jajdorkster@gmail.com>
2016-09-26 13:22:05 -0400

    Fix possible NULL dereference in MenuPowers.cpp

--- src/MenuPowers.cpp
+++ src/MenuPowers.cpp
@@ -1186,7 +1186,7 @@ void MenuPowers::logic() {
 	tablist.logic();
 	if (!tabs.empty()) {
 		for (size_t i=0; i<tabs.size(); i++) {
-			if (tab_control->getActiveTab() == static_cast<int>(i)) {
+			if (tab_control && tab_control->getActiveTab() == static_cast<int>(i)) {
 				tablist.setNextTabList(&tablist_pow[i]);
 			}
 			tablist_pow[i].logic();
