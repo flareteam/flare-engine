0c7fea3e44c909104bde071ca1dc077265c1c681
Justin Jacobs <jajdorkster@gmail.com>
2017-06-02 10:03:07 -0400

    Improve aim_assist documentation

--- src/PowerManager.cpp
+++ src/PowerManager.cpp
@@ -309,7 +309,7 @@ void PowerManager::loadPowers() {
 			// @ATTR power.visual_option|int|The animation sprite sheet containers rows of similar effects, use a specific option.
 			powers[input_id].visual_option = toInt(infile.val);
 		else if (infile.key == "aim_assist")
-			// @ATTR power.aim_assist|bool|Power is aim assisted.
+			// @ATTR power.aim_assist|bool|If true, power targeting will be offset vertically by the number of pixels set with "aim_assist" in engine/misc.txt.
 			powers[input_id].aim_assist = toBool(infile.val);
 		else if (infile.key == "speed")
 			// @ATTR power.speed|float|The speed of missile hazard, the unit is defined as map units per frame.
