flareteam/flare-engine

commit e5988cad2dbb5454919fcf1367f4175cf8d7efaa

Only activate buttons/checkboxes when cursor is inside bounding box
This is akin to most desktop widget toolkits where the user can click on
a button, but move the mouse off of the button before releasing to
cancel the action.

src/WidgetButton.cpp
@@ -93,7 +93,7 @@ bool WidgetButton::checkClick(int x, int y) {
 	if (inpt->lock[ACCEPT]) return false;
 
 	// main click released, so the button state goes back to unpressed
-	if (pressed && !inpt->lock[MAIN1] && !inpt->lock[ACCEPT]) {
+	if (pressed && !inpt->lock[MAIN1] && !inpt->lock[ACCEPT] && isWithinRect(pos, mouse)) {
 		pressed = false;
 		return true;
 	}

src/WidgetCheckBox.cpp
@@ -83,11 +83,13 @@ bool WidgetCheckBox::checkClick (int x, int y) {
 	if (inpt->lock[MAIN1]) return false;
 	if (inpt->lock[ACCEPT]) return false;
 
-	if (pressed && !inpt->lock[MAIN1] && !inpt->lock[ACCEPT]) { // this is a button release
+	if (pressed && !inpt->lock[MAIN1] && !inpt->lock[ACCEPT] && isWithinRect(pos, mouse)) { // this is a button release
 		pressed = false;
 		toggleCheck();
 		return true;
 	}
+
+	pressed = false;
 
 	if (inpt->pressing[MAIN1]) {
 		if (isWithinRect(pos, mouse)) {

commit 365140948d584db3cd4e3629090c5c0e88ab3cf3

Fix button/checkbox regression in e5988cad
Interacting with these widgets via keyboard/controller was broken
because the mouse cursor doesn't enter the bounding box in those cases.

src/Version.cpp
@@ -27,7 +27,7 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 #include <sstream>
 #include <iomanip>
 
-Version ENGINE_VERSION(1, 0, 8);
+Version ENGINE_VERSION(1, 0, 9);
 Version VERSION_MIN(0, 0, 0);
 Version VERSION_MAX(USHRT_MAX, USHRT_MAX, USHRT_MAX);
 

src/WidgetButton.cpp
@@ -37,6 +37,7 @@ WidgetButton::WidgetButton(const std::string& _fileName)
 	, tip_buf()
 	, tip_new()
 	, tip(new WidgetTooltip())
+	, activated(false)
 	, label("")
 	, tooltip("")
 	, enabled(true)
@@ -48,6 +49,7 @@ WidgetButton::WidgetButton(const std::string& _fileName)
 
 void WidgetButton::activate() {
 	pressed = true;
+	activated = true;
 }
 
 void WidgetButton::setPos(int offset_x, int offset_y) {
@@ -93,7 +95,8 @@ bool WidgetButton::checkClick(int x, int y) {
 	if (inpt->lock[ACCEPT]) return false;
 
 	// main click released, so the button state goes back to unpressed
-	if (pressed && !inpt->lock[MAIN1] && !inpt->lock[ACCEPT] && isWithinRect(pos, mouse)) {
+	if (pressed && !inpt->lock[MAIN1] && !inpt->lock[ACCEPT] && (isWithinRect(pos, mouse) || activated)) {
+		activated = false;
 		pressed = false;
 		return true;
 	}

src/WidgetButton.h
@@ -54,6 +54,8 @@ private:
 	WidgetTooltip *tip;
 	TooltipData checkTooltip(const Point& mouse);
 
+	bool activated;
+
 public:
 	explicit WidgetButton(const std::string& _fileName = "images/menus/buttons/button_default.png");
 	~WidgetButton();

src/WidgetCheckBox.cpp
@@ -35,6 +35,7 @@ WidgetCheckBox::WidgetCheckBox (const std::string &fname)
 	, cb(NULL)
 	, checked(false)
 	, pressed(false)
+	, activated(false)
 {
 	focusable = true;
 
@@ -52,6 +53,7 @@ WidgetCheckBox::WidgetCheckBox (const std::string &fname)
 
 void WidgetCheckBox::activate() {
 	pressed = true;
+	activated = true;
 }
 
 WidgetCheckBox::~WidgetCheckBox () {
@@ -86,7 +88,8 @@ bool WidgetCheckBox::checkClick (int x, int y) {
 	if (inpt->lock[MAIN1]) return false;
 	if (inpt->lock[ACCEPT]) return false;
 
-	if (pressed && !inpt->lock[MAIN1] && !inpt->lock[ACCEPT] && isWithinRect(pos, mouse)) { // this is a button release
+	if (pressed && !inpt->lock[MAIN1] && !inpt->lock[ACCEPT] && (isWithinRect(pos, mouse) || activated)) { // this is a button release
+		activated = false;
 		pressed = false;
 		toggleCheck();
 		return true;

src/WidgetCheckBox.h
@@ -52,6 +52,7 @@ private:
 	Sprite *cb;
 	bool checked;
 	bool pressed;
+	bool activated;
 };
 
 #endif