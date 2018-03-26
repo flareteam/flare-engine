0016aae6dc6a9a6e240e14f15f5e85dea58c55f7
Justin Jacobs <jajdorkster@gmail.com>
2018-03-26 13:07:36 -0400

    Add Linux wrapper script to distribution directory

---
+++ distribution/linux/flare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Flare Shell Script
+# Written by Ethan "flibitijibibo" Lee
+
+# Move to script's directory
+cd "`dirname "$0"`"
+
+# Get the kernel/architecture information
+UNAME=`uname`
+ARCH=`uname -m`
+
+# Set the libpath and pick the proper binary
+if [ "$UNAME" == "Linux" ]; then
+    if [ "$ARCH" == "x86_64" ]; then
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./x86_64/
+        ./x86_64/flare.x86_64
+    #else
+    #    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./x86/
+    #    ./x86/flare.x86
+    fi
+fi
