flareteam/flare-engine

commit 5a4e26ac552c7c2cc5869755005c49c17e6c4243

Fix #1634, crash in Mod::operator=

src/ModManager.cpp
@@ -81,14 +81,20 @@ Mod& Mod::operator=(const Mod &mod) {
 	*engine_max_version = *mod.engine_max_version;
 	depends = mod.depends;
 
-	depends_min.resize(mod.depends_min.size());
+	depends_min.resize(mod.depends_min.size(), NULL);
 	for (size_t i = 0; i < depends_min.size(); ++i) {
-		*depends_min[i] = *mod.depends_min[i];
+		if (depends_min[i])
+			*depends_min[i] = *mod.depends_min[i];
+		else
+			depends_min[i] = new Version(*mod.depends_min[i]);
 	}
 
-	depends_max.resize(mod.depends_max.size());
+	depends_max.resize(mod.depends_max.size(), NULL);
 	for (size_t i = 0; i < depends_max.size(); ++i) {
-		*depends_max[i] = *mod.depends_max[i];
+		if (depends_max[i])
+			*depends_max[i] = *mod.depends_max[i];
+		else
+			depends_max[i] = new Version(*mod.depends_max[i]);
 	}
 
 	assert(depends.size() == depends_min.size());

commit 5ec651c71cf096e9ff0ced0fc920110e8d872416

Fix small memory leak introduced in 5a4e26ac

src/ModManager.cpp
@@ -81,20 +81,20 @@ Mod& Mod::operator=(const Mod &mod) {
 	*engine_max_version = *mod.engine_max_version;
 	depends = mod.depends;
 
+	for (size_t i = 0; i < depends_min.size(); ++i) {
+		delete depends_min[i];
+	}
 	depends_min.resize(mod.depends_min.size(), NULL);
 	for (size_t i = 0; i < depends_min.size(); ++i) {
-		if (depends_min[i])
-			*depends_min[i] = *mod.depends_min[i];
-		else
-			depends_min[i] = new Version(*mod.depends_min[i]);
+		depends_min[i] = new Version(*mod.depends_min[i]);
 	}
 
+	for (size_t i = 0; i < depends_max.size(); ++i) {
+		delete depends_max[i];
+	}
 	depends_max.resize(mod.depends_max.size(), NULL);
 	for (size_t i = 0; i < depends_max.size(); ++i) {
-		if (depends_max[i])
-			*depends_max[i] = *mod.depends_max[i];
-		else
-			depends_max[i] = new Version(*mod.depends_max[i]);
+		depends_max[i] = new Version(*mod.depends_max[i]);
 	}
 
 	assert(depends.size() == depends_min.size());

src/Version.cpp
@@ -27,7 +27,7 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 #include <sstream>
 #include <iomanip>
 
-Version ENGINE_VERSION(1, 2, 0);
+Version ENGINE_VERSION(1, 2, 1);
 Version VERSION_MIN(0, 0, 0);
 Version VERSION_MAX(USHRT_MAX, USHRT_MAX, USHRT_MAX);
 