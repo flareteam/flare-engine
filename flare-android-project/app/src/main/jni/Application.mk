bddf8b795b461cd9f48e076b3d60c1c589d76a4d
Justin Jacobs <jajdorkster@gmail.com>
2018-05-10 16:23:19 -0400

    Android: Raise min SDK version to 14, drop armeabi support
    As of NDK version 17, support for SDK version 13 and armeabi have been
    deprecated.

--- flare-android-project/app/src/main/jni/Application.mk
+++ flare-android-project/app/src/main/jni/Application.mk
@@ -1,9 +1,4 @@
-
-# Uncomment this if you're using STL in your project
-# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-# APP_STL := stlport_static
-
-APP_ABI := armeabi armeabi-v7a x86
-APP_STL := stlport_static
-APP_PLATFORM := android-13
-NDK_TOOLCHAIN_VERSION=4.9
+APP_ABI := armeabi-v7a x86
+APP_STL := c++_static
+APP_PLATFORM := android-14
+APP_CPPFLAGS := -frtti
