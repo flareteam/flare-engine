c4da84d9ba669939bdb2147d31b41ed460181e42
Justin Jacobs <jajdorkster@gmail.com>
2018-07-02 21:52:00 -0400

    ModManager: Use operator= in copy constructor

--- src/ModManager.cpp
+++ src/ModManager.cpp
@@ -51,29 +51,17 @@ Mod::~Mod() {
 }
 
 Mod::Mod(const Mod &mod) {
-	name = mod.name;
-	description = mod.description;
-	game = mod.game;
-	version = new Version(*mod.version);
-	engine_min_version = new Version(*mod.engine_min_version);
-	engine_max_version = new Version(*mod.engine_max_version);
-	depends = mod.depends;
+	version = new Version();
+	engine_min_version = new Version();
+	engine_max_version = new Version();
 
-	depends_min.resize(mod.depends_min.size());
-	for (size_t i = 0; i < depends_min.size(); ++i) {
-		depends_min[i] = new Version(*mod.depends_min[i]);
-	}
-
-	depends_max.resize(mod.depends_max.size());
-	for (size_t i = 0; i < depends_max.size(); ++i) {
-		depends_max[i] = new Version(*mod.depends_max[i]);
-	}
-
-	assert(depends.size() == depends_min.size());
-	assert(depends.size() == depends_max.size());
+	*this = mod;
 }
 
 Mod& Mod::operator=(const Mod &mod) {
+	if (this == &mod)
+		return *this;
+
 	name = mod.name;
 	description = mod.description;
 	game = mod.game;
