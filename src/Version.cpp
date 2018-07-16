e8d67a25a6a844d774d225ad63921881c539e1f5
Justin Jacobs <jajdorkster@gmail.com>
2018-07-16 03:21:47 -0400

    Version 1.06.12

--- src/Version.cpp
+++ src/Version.cpp
@@ -30,7 +30,7 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 
 #include <SDL.h>
 
-Version VersionInfo::ENGINE(1, 6, 11);
+Version VersionInfo::ENGINE(1, 6, 12);
 Version VersionInfo::MIN(0, 0, 0);
 Version VersionInfo::MAX(USHRT_MAX, USHRT_MAX, USHRT_MAX);
 
