f9ced86ca8306cba52eb06ddbcca59d9b0356944
Justin Jacobs <jajdorkster@gmail.com>
2018-12-27 10:31:47 -0500

    WidgetScrollBox: Clear tablist in clearChildWidgets()
    This fixes a crash in the dialog system.

--- src/WidgetScrollBox.cpp
+++ src/WidgetScrollBox.cpp
@@ -77,6 +77,7 @@ void WidgetScrollBox::addChildWidget(Widget* child) {
 void WidgetScrollBox::clearChildWidgets() {
 	currentChild = -1;
 	children.clear();
+	tablist.clear();
 }
 
 void WidgetScrollBox::scroll(int amount) {
