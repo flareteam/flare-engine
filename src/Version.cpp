23a358fffb77d99b011df395059ec504b86bd1ec
Justin Jacobs <jajdorkster@gmail.com>
2019-04-02 22:27:12 -0400

    Update RELEASE_NOTES.txt and bump version

--- src/Version.cpp
+++ src/Version.cpp
@@ -30,7 +30,7 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 
 #include <SDL.h>
 
-Version VersionInfo::ENGINE(1, 9, 24);
+Version VersionInfo::ENGINE(1, 9, 25);
 Version VersionInfo::MIN(0, 0, 0);
 Version VersionInfo::MAX(USHRT_MAX, USHRT_MAX, USHRT_MAX);
 
