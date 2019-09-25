c8621ac81327fa3d2a18a3ad49db39a059bd1184
Justin Jacobs <jajdorkster@gmail.com>
2019-09-25 11:22:49 -0400

    Fixed bug where window could be sized below the minimum after exiting FS
    This bug was noticed on macOS, but it could have affected other
    platforms as well. This commit ensures that the window will return to
    the minimum default size, and no smaller, after exiting fullscreen.

--- src/Version.cpp
+++ src/Version.cpp
@@ -30,7 +30,7 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 
 #include <SDL.h>
 
-Version VersionInfo::ENGINE(1, 11, 4);
+Version VersionInfo::ENGINE(1, 11, 5);
 Version VersionInfo::MIN(0, 0, 0);
 Version VersionInfo::MAX(USHRT_MAX, USHRT_MAX, USHRT_MAX);
 
