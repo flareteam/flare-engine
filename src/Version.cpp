739d36dae2421d876b5ccec28592ad0906ccff75
Justin Jacobs <jajdorkster@gmail.com>
2020-04-06 00:23:57 -0400

    Bump version to 1.11.15
    I've forgotten to bump the minor version for some of the recent changes,
    so I skipped ahead. Here are the commits where the missing minor
    versions would be:
    
    1.11.08 = c14d7b40
    1.11.09 = df0969d9
    1.11.10 = d9586ecd
    1.11.11 = 7e778ff1
    1.11.12 = ba90d64a
    1.11.13 = e7a4702b
    1.11.14 = 75481151

--- src/Version.cpp
+++ src/Version.cpp
@@ -30,7 +30,7 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 
 #include <SDL.h>
 
-Version VersionInfo::ENGINE(1, 11, 7);
+Version VersionInfo::ENGINE(1, 11, 15);
 Version VersionInfo::MIN(0, 0, 0);
 Version VersionInfo::MAX(USHRT_MAX, USHRT_MAX, USHRT_MAX);
 
