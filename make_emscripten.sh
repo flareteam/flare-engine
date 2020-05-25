175eb1b80007cab1a9b99b12afc9596512027b09
Justin Jacobs <jajdorkster@gmail.com>
2020-05-25 01:04:18 -0400

    Fix mixed tabs and spaces in make_emscripten.sh

--- make_emscripten.sh
+++ make_emscripten.sh
@@ -10,20 +10,20 @@ cmake .
 
 emcc \
 	-v \
-    -Isrc/ \
-    src/*.cpp \
-    -O3 \
-    -s ASSERTIONS=1 \
-    -s ALLOW_MEMORY_GROWTH=1 \
-    -s USE_SDL=2 \
-    -s USE_SDL_IMAGE=2 \
-    -s SDL2_IMAGE_FORMATS='["png"]' \
+	-Isrc/ \
+	src/*.cpp \
+	-O3 \
+	-s ASSERTIONS=1 \
+	-s ALLOW_MEMORY_GROWTH=1 \
+	-s USE_SDL=2 \
+	-s USE_SDL_IMAGE=2 \
+	-s SDL2_IMAGE_FORMATS='["png"]' \
 	-s USE_SDL_TTF=2 \
 	-s USE_SDL_MIXER=2 \
 	-s "EXTRA_EXPORTED_RUNTIME_METHODS=['print']" \
 	-lidbfs.js \
-    --preload-file mods \
-    -o emscripten/index.html
+	--preload-file mods \
+	-o emscripten/index.html
 
 rm -rf mods/fantasycore
 rm -rf mods/empyrean_campaign
