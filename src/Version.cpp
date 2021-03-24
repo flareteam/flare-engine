e416b470e4c73503a26e065ccf67c3f184d54750
Justin Jacobs <jajdorkster@gmail.com>
2021-03-23 22:31:09 -0400

    Update RELEASE_NOTES.txt and bump version

--- src/Version.cpp
+++ src/Version.cpp
@@ -30,7 +30,7 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 
 #include <SDL.h>
 
-Version VersionInfo::ENGINE(1, 11, 70);
+Version VersionInfo::ENGINE(1, 11, 71);
 Version VersionInfo::MIN(0, 0, 0);
 Version VersionInfo::MAX(USHRT_MAX, USHRT_MAX, USHRT_MAX);
 
