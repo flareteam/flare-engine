aab6d983fb0a22dde7d8c4eea2573130be7fed7d
Justin Jacobs <jajdorkster@gmail.com>
2021-06-08 20:46:58 -0400

    Android: Request storage perms for newer APIs & implement getLibraries()

--- flare-android-project/app/src/main/java/org/flare/app/FLARE.java
+++ flare-android-project/app/src/main/java/org/flare/app/FLARE.java
@@ -1,6 +1,9 @@
 package org.flare.app; 
 
 import org.libsdl.app.SDLActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.*;
 
 /* 
@@ -8,11 +11,31 @@ import android.os.*;
  */ 
 
 public class FLARE extends SDLActivity { 
-    protected void onCreate(Bundle savedInstanceState) { 
+    protected void onCreate(Bundle savedInstanceState) {
+        // wait until we have storage permission to start the game
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            super.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            while (super.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                android.os.SystemClock.sleep(1000);
+            }
+        }
         super.onCreate(savedInstanceState);
-    } 
+    }
     
     protected void onDestroy() { 
         super.onDestroy(); 
+    }
+
+    protected String[] getLibraries() {
+        return new String[] {
+                "hidapi",
+                "SDL2",
+                "SDL2_image",
+                "mpg123",
+                "SDL2_mixer",
+                //"SDL2_net",
+                "SDL2_ttf",
+                "main"
+        };
     }
 }
