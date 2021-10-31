b653dd907d9ed86c26c84339483d383382f6e11b
Justin Jacobs <jajdorkster@gmail.com>
2021-10-31 14:04:57 -0400

    Disable markdown's "smarty" flag for doc gen

--- docs/gen/attribute-reference.sh
+++ docs/gen/attribute-reference.sh
@@ -4,9 +4,9 @@ HTMLFILE="../attribute-reference.html"
 PAGETITLE="Attribute Reference"
 
 sed -e "s/PAGETITLE/$PAGETITLE/g" header.txt > "$HTMLFILE"
-markdown "attribute-reference.md" >> "$HTMLFILE"
+markdown -f -smarty "attribute-reference.md" >> "$HTMLFILE"
 cd ../../
-./extract_xml.sh | xsltproc wiki.xslt - | sed -e '1,3d' | markdown >> "docs/gen/$HTMLFILE"
+./extract_xml.sh | xsltproc wiki.xslt - | sed -e '1,3d' | markdown -f -smarty >> "docs/gen/$HTMLFILE"
 
 
 cd "docs/gen"
--- docs/gen/code-conventions.sh
+++ docs/gen/code-conventions.sh
@@ -6,7 +6,7 @@ WIKIPATH="../../../flare-engine.wiki"
 
 if [ -d "$WIKIPATH" ]; then
 	sed -e "s/PAGETITLE/$PAGETITLE/g" header.txt > "$HTMLFILE"
-	markdown "$WIKIPATH/Code-Conventions.md" >> "$HTMLFILE"
+	markdown -f -smarty "$WIKIPATH/Code-Conventions.md" >> "$HTMLFILE"
 	cat footer.txt >> "$HTMLFILE"
 fi
 
