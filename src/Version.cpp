1686f3841c8e8ea36482df5da1a917ec79a02979
Justin Jacobs <jajdorkster@gmail.com>
2025-11-12 10:11:08 -0500

    SDLInputState: Update gamepad bindings for accessing menus
    Instead of mapping menus to L3/R3 (which are easy to click accidentally
    during regular gameplay), we use the new cycle menus binding on the
    Back/Select button.

--- src/Version.cpp
+++ src/Version.cpp
@@ -30,7 +30,7 @@ FLARE.  If not, see http://www.gnu.org/licenses/
 
 #include <SDL.h>
 
-Version VersionInfo::ENGINE(1, 14, 216);
+Version VersionInfo::ENGINE(1, 14, 217);
 Version VersionInfo::MIN(0, 0, 0);
 Version VersionInfo::MAX(USHRT_MAX, USHRT_MAX, USHRT_MAX);
 
